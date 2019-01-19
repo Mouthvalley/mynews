670b76b10fb39a8826fd980fdc862d6d2f154577
谷口　大和 <taniguchiyamato@taniguchiyamato-no-MacBook-Air.local>
2019-01-19 16:24:44 +0900

    Laravel 11 更新

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>MyNews</title>
-  </head>
-  <body>
-    <h1>Myニュース作成画面</h1>
-  </body>
-</html>
-
-@extends('layouts.admim')
+@extends('layout.admin')
 
 @section('title','ニュースの新規作成')
 
