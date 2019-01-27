db775c13a4eff08ba40c82a42fdeea31c6d17233
谷口　大和 <taniguchiyamato@taniguchiyamato-no-MacBook-Air.local>
2019-01-27 11:55:57 +0900

    Laravel15

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -8,13 +8,13 @@
                 <h2>ニュース新規作成</h2>
                 <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
 
-                    @if (count($errors) > 0)
-                        <ul>
-                            @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
+                  @if (count($errors) > 0)
+                    <ul>
+                        @foreach($errors->all() as $e)
+                            <li>{{ $e }}</li>
+                        @endforeach
+                    </ul>                    
+                  @endif
                     <div class="form-group row">
                         <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
@@ -35,8 +35,8 @@
                     </div>
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="更新">
-                </form>
-            </div>
-        </div>
-    </div>
+                  </form>
+              </div>
+          </div>
+      </div>
 @endsection
