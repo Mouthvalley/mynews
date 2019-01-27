138e3249fa52f92632118f61e999254a0e532ab2
谷口　大和 <taniguchiyamato@taniguchiyamato-no-MacBook-Air.local>
2019-01-27 18:14:29 +0900

    Laravel15 課題5 6

--- app/profiles.php
+++ app/profiles.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class profiles extends Model
 {
-    //
+  public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
 }
